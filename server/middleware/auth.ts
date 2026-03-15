53ead9b06bb2d6b3a3770fc6c3e8daa7f5305702
senghong <senghong.learning@gmail.com>
2026-03-15 12:35:39 +0700

    Add api authentication verify

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,35 +1,42 @@
+
+
+import { getHeader } from 'h3';
 import tokenService from "../services/tokenService";
 
 export default defineEventHandler(async (event) => {
-//   const authHeader = event.req.headers['authorization'] || event.req.headers['Authorization'] as string;
-//     if (!authHeader) { 
-//         throw createError({
-//             statusCode: 401,
-//             statusMessage: 'Authorization header missing',
-//         });  
-//     }
+  const ignoredPaths = [
+    '/api/auth/login',
+    '/api/auth/register',
+  ];
+  if (ignoredPaths.includes(event.node.req.url || '') || (!event.node.req.url?.includes('/api'))) {
+    return;
+  }
+  const authHeader = getHeader(event, 'authorization');
 
-//     const token = authHeader.split(' ')[1];
-//     if (!token) {
-//         throw createError({
-//             statusCode: 401,
-//             statusMessage: 'Token missing',
-//         });
-//     }
+  let token: string | undefined;
+  if (authHeader && typeof authHeader === 'string') {
+    token = authHeader.split(' ')[1];
+  }
+  if (!token) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: 'Token missing',
+    });
+  }
 
-//     try {
-//         const decoded = tokenService.verifyToken(token);
-//         if (!decoded) {
-//             throw createError({
-//                 statusCode: 401,
-//                 statusMessage: 'Invalid token',
-//             });
-//         }   
-//         event.context.userId = decoded.id;
-//     } catch (error) {
-//         throw createError({
-//             statusCode: 401,
-//             statusMessage: 'Invalid token',
-//         });
-//     }
+  try {
+    const decoded = tokenService.verifyToken(token);
+    if (!decoded) {
+      throw createError({
+        statusCode: 401,
+        statusMessage: 'Invalid token',
+      });
+    }
+    event.context.user = decoded;
+  } catch (error) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: 'Invalid token',
+    });
+  }
 });
